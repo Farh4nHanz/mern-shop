b2c86cf2dee8b282aae125664027ea888da7ec04
Farh4nHanz <f620031718@gmail.com>
2024-10-17 15:21:48 +0700

    Update API endpoint

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -16,7 +16,7 @@ export default defineConfig({
   server: {
     proxy: {
       "/api": {
-        target: process.env.VITE_API_URL,
+        target: process.env.VITE_API_URL || import.meta.env.VITE_API_URL,
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, ""),
       },
