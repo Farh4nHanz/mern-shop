02dd06371ff5bf8308af69c84f5396c22c13a0dd
Farh4nHanz <f620031718@gmail.com>
2024-10-17 14:53:43 +0700

    Updated project structure

--- backend/server.js
+++ backend/server.js
@@ -1,19 +1,12 @@
 import connectDB from "./config/db.js";
 import e from "express";
-import path from "path";
 import cors from "cors";
 import morgan from "morgan";
 
 const app = e();
 const port = process.env.PORT;
 
-const __dirname = path.resolve();
-
-app.use(
-  cors({
-    origin: "https://main.d14tynfj08syl2.amplifyapp.com/",
-  })
-);
+app.use(cors());
 
 app.use(e.json());
 app.use(e.urlencoded({ extended: false }));
@@ -33,13 +26,6 @@ process.env.NODE_ENV === "development"
 import productRoutes from "./routes/productRoutes.js";
 
 app.use("/api/v1/products", productRoutes);
-
-if (process.env.NODE_ENV === "production") {
-  app.use(e.static(path.join(__dirname, "/frontend/dist")));
-  app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
-  });
-}
 
 app.listen(port, () => {
   connectDB();
